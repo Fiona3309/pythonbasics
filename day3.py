319518bcd1b518019c6995505ba909939924f07d
Ramirez <FKRamirez@cps.k12.il.us>
2024-04-29 08:40:45 -0500

    learning basics

---
+++ day3.py
@@ -0,0 +1,26 @@
+#boolean
+print(10>9)
+broughtFood= True
+print(broughtFood)
+is_raining = True
+if is_raining:
+    print("Take an umbrella")
+else:
+    print("No umbrella needed")
+temperature = 35
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+    print("Done")
+elif temperature > 20:
+    print("it's nice")
+else:
+    print("it's cold")
+    print("Done")
+age = 22
+if age >= 18:
+        message = "Eligible"
+else:
+        message = "Not eligible"
+        
+        print(message)
---
+++ main.py
@@ -0,0 +1,57 @@
+# print("hello world")
+# #print is a funtion that prints out a message to the console
+# #strings are surrounded by quotes 
+# #single or double quotes '' or ""
+# # whenever a word is surrounded by quotes it is called a string 
+# # be consistent with the quotes you use
+# print("Fiona")
+# print("order of execution")
+# print("in python")
+# print("*"*20)
+# #variables are used to store data
+# #variables are created when you assign a value to it
+# #variables are case sensiitve
+# price=10 #Integer
+# name="john" #string variable
+# rating = 4.9 #float variable
+# is_published= True #boolean variable
+# print(name) 
+# print(price)
+# print(rating)
+# print(is_published)
+# #string interpolation where you can use variables in a sentence
+# print(name+ "is a basketball player")
+# print(name + "has a rating of" +str(rating))
+# #use str() function to convert a number to a string 
+# #this is called type conversion
+# print("the price of the book is"+ str(price))
+# #getting input from the user
+
+name = input ("what is your name")
+age= input (" what is your age")
+occupation = input ("what is occupation")
+print("hello" + name + "you are" + age + "years old and you are a" + occupation)
+
+
+score = int(input("Enter your score:"))
+if score >= 90:
+    print("A")
+elif score >= 80 and score < 90:
+    print("B")
+elif score >= 70 and score < 79:
+    print("C")
+elif score >= 60 and score < 69:
+    print("D")
+elif score =< 60
+    print("f")
+#Create a Python script that assigns a grade to a student based on their exam score.
+#Create a Python script that assigns a grade to a student based on their exam score.
+# Ask the user to input a score. Assume the score is out of 100.
+# Implement the logic to determine the grade based on the score:
+# A score of 90 and above is an "A".
+# A score of 80 to 89 is a "B".
+# A score of 70 to 79 is a "C".
+# A score of 60 to 69 is a "D".
+# A score below 60 is an "F".
+# Ensure the score entered is within the valid range (0-100). If not, print an error message.
+
