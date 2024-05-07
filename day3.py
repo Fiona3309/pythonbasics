ba794ab4d6def655afc90d5fd0d3716a2b9f1d63
Ramirez <FKRamirez@cps.k12.il.us>
2024-05-07 09:14:57 -0500

    loops2

--- day3.py
+++ day3.py
@@ -1,26 +1,58 @@
-#boolean
-print(10>9)
-broughtFood= True
-print(broughtFood)
-is_raining = True
-if is_raining:
-    print("Take an umbrella")
-else:
-    print("No umbrella needed")
-temperature = 35
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-    print("Done")
-elif temperature > 20:
-    print("it's nice")
-else:
-    print("it's cold")
-    print("Done")
-age = 22
-if age >= 18:
-        message = "Eligible"
-else:
-        message = "Not eligible"
+# #boolean
+# print(10>9)
+# broughtFood= True
+# print(broughtFood)
+# is_raining = True
+# if is_raining:
+#     print("Take an umbrella")
+# else:
+#     print("No umbrella needed")
+# temperature = 35
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+#     print("Done")
+# elif temperature > 20:
+#     print("it's nice")
+# else:
+#     print("it's cold")
+#     print("Done")
+# age = 22
+# if age >= 18:
+#         message = "Eligible"
+# else:
+#         message = "Not eligible"
         
-        print(message)
+#         print(message)
+# f age >= 15 \
+#     print("yes")
+# is_registered = True
+# allergies = input("yes or no")
+# if yes: print ("what food?")
+# weathersunny = True
+# if = sunny: print ("bring sunscreen")
+# elif = rain: print ("bring umbrella")
+# else: print ("weather is goog, bring nothing")
+
+name = input("Enter your name:")
+while name == "":
+    print("You did not enter your name")
+    name= input("enter your name")
+
+    age = int(input("Enter your age:"))
+    while age < 0:
+        print(f"you are (age) years old")
+
+        food = input("enter a food you like")
+
+        while food == "q":
+            print(f"You like (food)")
+            food = input ("enter your name")
+
+            num = input("enter a number between 1 - 10")
+            while our (num)< 1 or num > 10:
+                
+                      print (f"(num)"is not valid)
+print("your number is (num)")
+#wildloops
+
---
+++ whileReview.py
@@ -0,0 +1,47 @@
+# while loo[s allow you to repeat code until is met 
+# while loops are defined with the while keyboard
+#while loops are used when you don't know how many
+#times you need to repeat the code
+
+# studentList = {"Citlali", "Praise", "Aaron","Fiona","xavier"}
+# studentName = 0
+# while studentName < len(studentList): 
+#     print(studentList[studentName])
+#     studentName += 1
+
+# fruitList = ["apple", "banana", "cherry","date","elderberry"]
+# for fruit in fruitList:
+#     print (fruit + "is a fruit.")
+
+# for student in studentList:
+# studentList = ["Citlali", "Praise", "Aaron","Fiona","xavier"]
+# ask = input("what is your last name?")
+# print (student + "is a student.")
+# nums = range (1,100)     
+# for num in nums:
+#     for letter in 'abc':
+#        for letter2 in 'abc':
+#            for letter3 in 'abc':
+#               for letter4 in 'abc':
+#     # if num == 3:
+#     #     # print('Found!')
+#     #     continue
+#                       print(num,letter,letter2,letter3,letter4)                       
+#while loop - when you dont know the number of iterations0.
+# for i in range(20):
+#     print(i)
+# x = 0
+# while x < 10:
+#     if x == 5:
+#        print(x)
+#     x += 1
+while True:
+    print("Fiona:")
+    name = input()
+    if name == 'Fiona':
+        name = input()
+        print("Fiona" + name)
+        break
+    else:
+        print("thank you")
+    
